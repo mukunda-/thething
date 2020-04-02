311ad20f596e82722e8fc3108390e2393ab5fc86
Mukunda Johnson <mukunda@mukunda.com>
2020-04-01 21:35:56 -0500

    browser test

--- index.test.ts
+++ index.test.ts
@@ -1,5 +1,31 @@
 import { multiply } from './index';
+const playwright = require('playwright');
 
 test( 'multiply tester', async () => {
    expect( multiply( 3, 7 ) ).toEqual( 21 );
+});
+
+test( 'mf test', async() => {
+   
+   for (const browserType of ['chromium', 'firefox', 'webkit']) {
+      console.log( browserType, "START" );
+      const browser = await playwright[browserType].launch();
+      console.log( browserType, "LAUNCHED" );
+      const context = await browser.newContext();
+      console.log( browserType, "MADE CONTEXT" );
+      const page = await context.newPage();
+      console.log( browserType, "NEW PAGE" );
+      await page.goto('http://localhost:3000/');
+      console.log( browserType, "VISITED PAGE" );
+      await page.waitForSelector('#thebutton', { visible: true });
+      console.log( browserType, "FOUND SELECTOR" );
+      await page.click('#thebutton');
+      console.log( browserType, "CLICKED THE MF BUTTON" );
+      const result = await page.$eval('#answer', (el:any) => el.innerHTML);
+      console.log( browserType, "FETCHED THE RESULT" );
+      await browser.close();
+      console.log( browserType, "IS IT COFFEE?" );
+      expect(result).toEqual('coffee');
+      console.log( browserType, "I THINK SO" );
+   }
 });
--- webserver.ts
+++ webserver.ts
@@ -2,7 +2,23 @@ const express = require('express'),
       server  = express(),
       port    = 3000;
 
-server.get('/', (req:any, res:any) => res.send('Hello World!'));
+const testablePage = `<!doctype html>
+<html>
+  <head>
+    <script type="text/javascript">
+      function tmwiwfd() {
+        document.getElementById('wiwfd').innerHTML = '<p id="answer">coffee</p>';
+      }
+    </script>
+  </head>
+  <body>
+    <p>What do I want for dinner?</p>
+    <div id="wiwfd"></div>
+    <input type="button" id="thebutton" onclick="tmwiwfd()" value="Let's find out" />
+  </body>
+</html>`;
+
+server.get('/', (req:any, res:any) => res.send(testablePage));
 
 const page = `Example app listening at http://localhost:${port}`;
 
